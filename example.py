168e12cadfa34aefcdb1c2ce5ae3d6a3bf5e6aad
Renko <syokope@gmail.com>
2025-09-10 15:50:57 +0900

    ハッカソンPython課題を追加

---
+++ example.py
@@ -0,0 +1,27 @@
+# 問題1
+print("hello world")
+
+# 問題2
+def greet():
+    return "こんにちは"
+
+print(greet())
+
+# 問題3
+def print_name(name):
+    return f"私の名前は{name}です"
+
+print(print_name("れんこ"))
+
+# 問題4
+def get_greet():
+    return "おはようございます"
+
+print(get_greet())
+
+# 問題5
+def add(a, b):
+    return a + b
+
+result = add(5, 6)
+print(result)
